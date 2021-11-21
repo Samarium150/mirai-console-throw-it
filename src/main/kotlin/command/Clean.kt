85b185c5ef509472a97c8699796e8e77729864d9
Samarium <28302241+Samarium150@users.noreply.github.com>
2021-11-21 17:06:19 +0800

    refactor: extract command processing

--- src/main/kotlin/command/Clean.kt
+++ src/main/kotlin/command/Clean.kt
@@ -17,12 +17,12 @@
 package com.github.samarium150.command
 
 import com.github.samarium150.MiraiConsoleThrowIt
+import com.github.samarium150.Utils
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import java.io.File
-import java.util.*
 
 object Clean: SimpleCommand(
     MiraiConsoleThrowIt,
@@ -38,18 +38,15 @@ object Clean: SimpleCommand(
     @Handler
     suspend fun CommandSender.handle() {
         val dir = File(MiraiConsoleThrowIt.dataPath)
-        if (dir.exists() && dir.isDirectory) {
-            try {
-                Arrays.stream(dir.listFiles()).forEachOrdered {
-                    if (it.isFile) {
-                        it.delete()
-                    }
+        Utils.cleanupDirectory(dir)
+            .onFailure { exception ->
+                run {
+                    MiraiConsoleThrowIt.logger.error(exception)
+                    sendMessage("清理失败了，查看日志获取更多信息")
                 }
-                sendMessage("清理成功")
-            } catch (e: Exception) {
-                MiraiConsoleThrowIt.logger.error(e)
-                sendMessage("清理失败了，查看日志获取更多信息")
             }
-        }
+            .onSuccess {
+                sendMessage("清理成功")
+            }
     }
 }
--- src/main/kotlin/command/Throw.kt
+++ src/main/kotlin/command/Throw.kt
@@ -17,17 +17,13 @@
 package com.github.samarium150.command
 
 import com.github.samarium150.MiraiConsoleThrowIt
-import net.coobird.thumbnailator.Thumbnails
-import net.coobird.thumbnailator.geometry.Positions
+import com.github.samarium150.Utils
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.command.descriptor.ExperimentalCommandDescriptors
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 import net.mamoe.mirai.contact.Contact.Companion.uploadImage
 import net.mamoe.mirai.contact.User
-import java.awt.AlphaComposite
-import java.awt.RenderingHints
-import java.awt.geom.Ellipse2D
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.FileNotFoundException
@@ -44,7 +40,7 @@ object Throw: SimpleCommand(
     @ExperimentalCommandDescriptors
     override val prefixOptional: Boolean = true
 
-    private val template: BufferedImage
+    val template: BufferedImage
 
     init {
         val dir = File(MiraiConsoleThrowIt.dataPath)
@@ -61,45 +57,13 @@ object Throw: SimpleCommand(
         val resultPath = MiraiConsoleThrowIt.dataPath + target.id + ".png"
         val result = File(resultPath)
         if (!result.exists()) {
-            try {
-                @Suppress("BlockingMethodInNonBlockingContext")
-                val avatarUrl = URL(target.avatarUrl)
-                @Suppress("BlockingMethodInNonBlockingContext")
-                val avatar = ImageIO.read(avatarUrl)
-
-                var processedAvatar = BufferedImage(avatar.width, avatar.height, BufferedImage.TYPE_INT_ARGB)
-                var graphics = processedAvatar.createGraphics()
-                graphics.setRenderingHints(
-                    RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-                )
-                graphics.clip = Ellipse2D.Double(0.0, 0.0, avatar.width.toDouble(), avatar.height.toDouble())
-                graphics.drawImage(avatar, 0, 0, null)
-                graphics.dispose()
-
-                @Suppress("BlockingMethodInNonBlockingContext")
-                processedAvatar = Thumbnails.of(processedAvatar)
-                    .size(136, 136)
-                    .rotate(-160.0)
-                    .asBufferedImage()
-                @Suppress("BlockingMethodInNonBlockingContext")
-                processedAvatar = Thumbnails.of(processedAvatar)
-                    .sourceRegion(Positions.CENTER, 136, 136)
-                    .size(136, 136)
-                    .keepAspectRatio(false)
-                    .asBufferedImage()
-                graphics = template.createGraphics()
-                graphics.composite = AlphaComposite.getInstance(AlphaComposite.SRC_ATOP)
-                graphics.drawImage(processedAvatar, 19, 181, 137, 137, null)
-                graphics.dispose()
-
-                @Suppress("BlockingMethodInNonBlockingContext")
-                Thumbnails.of(template)
-                    .size(512, 512)
-                    .toFile(resultPath)
-            } catch (e: Exception) {
-                MiraiConsoleThrowIt.logger.error(e)
-                sendMessage("丢失败了，查看日志获取更多信息")
-            }
+            Utils.processAvatar(target.avatarUrl, resultPath)
+                .onFailure { exception ->
+                    run {
+                        MiraiConsoleThrowIt.logger.error(exception)
+                        sendMessage("生成图片失败了，查看日志获取更多信息")
+                    }
+                }
         }
         if (result.exists()) {
             MiraiConsoleThrowIt.logger.info("丢出"+target.nick)
